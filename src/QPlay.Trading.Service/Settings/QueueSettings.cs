60295e886e2753798def78814c41350c7b866161
Shinmen Dokkodo <shinmen.dokkodo@gmail.com>
2023-07-12 00:01:52 +0530

    purchasestate, queuesettings classes added, extra line removed from getpurchasestate

---
+++ src/QPlay.Trading.Service/Settings/QueueSettings.cs
@@ -0,0 +1,8 @@
+namespace QPlay.Trading.Service.Settings;
+
+public class QueueSettings
+{
+    public string GrantItemsQueueAddress { get; init; }
+    public string DebitGilQueueAddress { get; init; }
+    public string SubtractItemsQueueAddress { get; init; }
+}
