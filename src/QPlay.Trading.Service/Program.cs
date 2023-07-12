d52ef1294e9852daba87eea37e5704009fcca5b9
Shinmen Dokkodo <shinmen.dokkodo@gmail.com>
2023-07-12 16:40:32 +0530

    .csproj files

--- src/QPlay.Trading.Service/Program.cs
+++ src/QPlay.Trading.Service/Program.cs
@@ -1,21 +1,26 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using QPlay.Common.Identity;
+using QPlay.Trading.Service.Extensions;
+using QPlay.Trading.Service.SignalR;
 
 namespace QPlay.Trading.Service;
 
-public class Program
+public static class Program
 {
     public static void Main(string[] args)
     {
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
         // Add services to the container.
-
-        builder.Services.AddControllers();
+        builder.Services.ConfigureMongo();
+        builder.Services.ConfigureAuthentication();
+        builder.Services.ConfigureMassTransit(builder.Configuration);
+        builder.Services.ConfigureControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        builder.Services.ConfigureSignalR();
 
         WebApplication app = builder.Build();
 
@@ -24,15 +29,14 @@ public class Program
         {
             app.UseSwagger();
             app.UseSwaggerUI();
+            app.ConfigureCors(builder.Configuration);
         }
 
         app.UseHttpsRedirection();
-
+        app.UseAuthentication();
         app.UseAuthorization();
-
-
         app.MapControllers();
-
+        app.MapHub<MessageHub>("/messageHub");
         app.Run();
     }
 }
